9dd58804d65f76c69463e920f211aa8789ad773d
hardikrana929 <hardikrana929@gmail.com>
2026-04-09 18:22:45 +0530

    add serach and prevent duplicate task features

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { v4 as uuidv4 } from "uuid";
-import '../App.css'
+import "../App.css";
 const Todos = () => {
   const [todoList, setTodoList] = useState(() => {
     const localData = localStorage.getItem("TodoList");
     return localData ? JSON.parse(localData) : [];
   });
   const [task, setTesk] = useState("");
+  const [search, setSearch] = useState("");
+
+  //Store data in localStorage
   useEffect(() => {
     localStorage.setItem("TodoList", JSON.stringify(todoList));
   }, [todoList]);
 
+  //Search Task Feture
+  const searchToDo = todoList.filter((each) =>
+    each.todoname.toLowerCase().includes(search.toLowerCase()),
+  );
   //Remove Task
   const handleDelete = (id) => {
     const filterList = todoList.filter((each) => each.id !== id);
@@ -32,6 +39,11 @@ const Todos = () => {
     });
     setTodoList(completeTask);
   };
+  //Check Duplicate
+  const isDuplicate = todoList.some(
+    (each) => each.todoname.toLowerCase() === task.trim().toLowerCase(),
+  );
+
   //Task Added
   const handleTodo = (e) => {
     e.preventDefault();
@@ -41,6 +53,13 @@ const Todos = () => {
         autoClose: 2000,
         theme: "colored",
       });
+    } else if (isDuplicate) {
+      toast.error("Task is Alrady Exist...", {
+        position: "top-left",
+        autoClose: 2000,
+        theme: "colored",
+      });
+      return;
     } else {
       const newTask = {
         id: uuidv4(),
@@ -89,32 +108,55 @@ const Todos = () => {
                   </button>
                 </div>
               </form>
-
+              {/* Search Task     */}
+              <div className="input-group mb-4">
+                <span className="input-group-text bg-white border-end-0 rounded-start-pill">
+                  <i className="bi bi-search"></i>
+                </span>
+                <input
+                  type="text"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="form-control border-start-0 rounded-end-pill"
+                  placeholder="Search tasks..."
+                />
+              </div>
               {/* Todo List */}
-              <ul className="list-group mt-4">
-                {todoList.map((each) => (
-                  <li
-                    key={each.id}
-                    className="list-group-item d-flex justify-content-between align-items-center"
-                  >
-                    <span className={each.complete ? "task-done" :""} >{each.todoname}</span>
-                    <div>
-                      <button
-                        className="btn btn-sm btn-outline-success me-2"
-                        onClick={() => handleComplete(each.id)}
-                      >
-                        <i class="bi bi-check2-circle"></i>
-                      </button>
-                      <button
-                        className="btn btn-sm btn-outline-danger"
-                        onClick={() => handleDelete(each.id)}
-                      >
-                        <i class="bi bi-trash"></i>
-                      </button>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              {searchToDo.length === 0 ? (
+                <img src="https://static.vecteezy.com/system/resources/previews/014/814/192/non_2x/creatively-designed-flat-conceptual-icon-of-no-task-vector.jpg" 
+                  alt="Task Not Found"
+                  width="300px"
+                  className="mx-auto d-block"
+                />
+                
+              ) : (
+                <ul className="list-group mt-4">
+                  {searchToDo.map((each) => (
+                    <li
+                      key={each.id}
+                      className="list-group-item d-flex justify-content-between align-items-center"
+                    >
+                      <span className={each.complete ? "task-done" : ""}>
+                        {each.todoname}
+                      </span>
+                      <div>
+                        <button
+                          className="btn btn-sm btn-outline-success me-2"
+                          onClick={() => handleComplete(each.id)}
+                        >
+                          <i className="bi bi-check2-circle"></i>
+                        </button>
+                        <button
+                          className="btn btn-sm btn-outline-danger"
+                          onClick={() => handleDelete(each.id)}
+                        >
+                          <i className="bi bi-trash"></i>
+                        </button>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
           </div>
         </div>
